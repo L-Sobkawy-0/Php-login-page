b21fb3aa1bb246f088e5112c2180fc11f36542ee
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-08-27 18:05:55 +0300

    Update Feedback.php

--- Feedback.php
+++ Feedback.php
@@ -1,7 +1,6 @@
 <?php
 include "connection.php";
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $feedback = $_POST["feedback"];
@@ -10,14 +9,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bind_param("ss", $name, $feedback);
     $stmt->execute();
 
-    echo "Feedback submitted!";
+    header("Location: admin.php");
+    exit();
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Feedback</title>
+</head>
+<body>
 
-<form method="POST">
+<form method="POST" action="feedback.php">
     <input type="text" name="name" placeholder="Your name">
     <br><br>
     <textarea name="feedback" placeholder="Write your feedback"></textarea>
     <br><br>
     <button type="submit">Send Feedback</button>
 </form>
+
+</body>
+</html>
