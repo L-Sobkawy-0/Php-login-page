c22dc227f4bc8b89368f8b05052b3e6ab8b8e8c1
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-08-31 21:20:35 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,10 +1,15 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
+    header("Location: index.php");
+    exit();
+}
+
 include "connection.php";
 $result = $conn->query("SELECT * FROM Feedback");
 ?>
-
 <h2>Admin - Feedback</h2>
-
 <?php
 while ($row = $result->fetch_assoc()) {
     echo "<b>" . htmlspecialchars($row["name"]) . "</b><br>";
