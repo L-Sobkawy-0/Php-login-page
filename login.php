3b68f83ad45eff12aeee8e176a0489e134027de4
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-08-31 13:53:55 +0300

    login.php

--- login.php
+++ login.php
@@ -5,20 +5,21 @@ error_reporting(E_ALL);
 include 'connection.php';
 session_start();
 
-print "<h1>This is a heading</h1>"; 
-$username = $_POST['username'];
-$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-$result = $conn->query($query);
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($query);
 
-if ($result->num_rows === 1) {
-    $user = $result->fetch_assoc();
-    $_SESSION['full_name'] = $user['username'];
-    header("Location: welcome.php");
-    exit();
-} else {
-    echo "Invalid username or password. <a href='index.php'>Try again</a>";
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        $_SESSION['full_name'] = $user['username'];
+        header("Location: welcome.php");
+        exit();
+    } else {
+        echo "Invalid username or password. <a href='index.php'>Try again</a>";
+    }
 }
 
 $conn->close();
