4f4586a79c0fc1aed973b64b3ed023d140168df9
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-09-03 18:11:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,22 @@
 <!DOCTYPE html>
 <html lang='en'>
-
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <title>Login page</title>
 </head>
-
 <body>
     <form action="login.php" method="post">
-<input name="username" type="text"username placeholder="username">
-<input name="password" type="password"password placeholder="password">
-<button>login</button>
+        <input name="username" type="text" placeholder="username">
+        <input name="password" type="password" placeholder="password">
+        <button type="submit">login</button>
     </form>
     <br>
-
     <a href="comments.php">
         <button type="button">Comments</button>
     </a>
-
     <a href="feedback.php">
         <button type="button">Feedback</button>
     </a>
-
 </body>
 </html>
-
-
