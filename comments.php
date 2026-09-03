a1c3295ea03559d7ce9320b0e0bed9311da87424
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-09-03 18:13:01 +0300

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,16 +1,22 @@
 <?php
 include "connection.php";
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_SESSION['logged_in'])) {
+        http_response_code(403);
+        exit("You must be logged in to post a comment.");
+    }
+
     $name = $_POST["name"];
     $comment = $_POST["comment"];
-
     $query = "INSERT INTO comments (name, comment) VALUES ('$name', '$comment')";
     $conn->query($query);
 }
 
 $result = $conn->query("SELECT * FROM comments");
 ?>
+<?php if (!empty($_SESSION['logged_in'])): ?>
 <form method="POST">
     <input type="text" name="name" placeholder="Your name">
     <br><br>
@@ -18,6 +24,9 @@ $result = $conn->query("SELECT * FROM comments");
     <br><br>
     <button type="submit">Post Comment</button>
 </form>
+<?php else: ?>
+    <p>Please <a href="index.php">log in</a> to post a comment.</p>
+<?php endif; ?>
 
 <h2>Comments</h2>
 <?php
