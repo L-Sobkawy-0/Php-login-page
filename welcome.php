524387effd66dc50c15ecbae0ce21e4af7398fce
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-09-03 18:10:31 +0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['full_name'])) {
+if (empty($_SESSION['logged_in']) || !isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
 }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,12 +14,10 @@ if (!isset($_SESSION['full_name'])) {
 </head>
 <body>
     <h1>Welcome, <?php echo htmlspecialchars($_SESSION['full_name']); ?>!</h1>
-
     <form method="GET">
         <input type="text" name="search_input">
         <button type="submit">Search</button>
     </form>
-
     <?php
     if (isset($_GET['search_input'])) {
         echo "Your input is: " . htmlspecialchars($_GET['search_input']);
