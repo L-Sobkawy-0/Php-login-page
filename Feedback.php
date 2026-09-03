00da0c0639431d9ad2425020fe962fd46d5c755a
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-09-03 18:14:16 +0300

    Update Feedback.php

--- Feedback.php
+++ Feedback.php
@@ -1,13 +1,17 @@
 <?php
 include "connection.php";
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_SESSION['logged_in'])) {
+        http_response_code(403);
+        exit("You must be logged in to submit feedback.");
+    }
+
     $name = $_POST["name"];
     $feedback = $_POST["feedback"];
-
     $query = "INSERT INTO Feedback (name, feedback) VALUES ('$name', '$feedback')";
     $conn->query($query);
-
     header("Location: admin.php");
     exit();
 }
@@ -18,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Feedback</title>
 </head>
 <body>
-
+<?php if (!empty($_SESSION['logged_in'])): ?>
 <form method="POST" action="feedback.php">
     <input type="text" name="name" placeholder="Your name">
     <br><br>
@@ -26,6 +30,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <br><br>
     <button type="submit">Send Feedback</button>
 </form>
-
+<?php else: ?>
+    <p>Please <a href="index.php">log in</a> to submit feedback.</p>
+<?php endif; ?>
 </body>
 </html>
