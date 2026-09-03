bdb979b44d86a385cda8641187497bfe4e976517
L-Sobkawy-0 <mo.ma.elsobki@gmail.com>
2026-09-03 15:16:31 +0300

    Update login.php

--- login.php
+++ login.php
@@ -8,19 +8,18 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
     $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($query);
 
     if ($result->num_rows === 1) {
         $user = $result->fetch_assoc();
         $_SESSION['full_name'] = $user['username'];
+        $_SESSION['is_admin'] = ($user['username'] === 'admin');
         header("Location: welcome.php");
         exit();
     } else {
         echo "Invalid username or password. <a href='index.php'>Try again</a>";
     }
 }
-
 $conn->close();
 ?>
